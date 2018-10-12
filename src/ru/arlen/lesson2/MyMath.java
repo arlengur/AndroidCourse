1978916b391b0ce65be3f7c391efb792a841d50e
angalin <ANGalin.SBT@sberbank.ru>
2018-10-12 09:38:52 +0300

    Lessons 1 and 2 is done.

--- src/ru/arlen/lesson2/MyMath.java
+++ src/ru/arlen/lesson2/MyMath.java
@@ -5,6 +5,10 @@ import java.util.Arrays;
 public class MyMath {
 
     private void printArr(int c, int d, int... a) {
+        if (c > d) {
+            System.out.println("Wrong interval c > d!");
+            return;
+        }
         if (a == null) {
             System.out.println("Array is null!");
             return;
@@ -16,6 +20,7 @@ public class MyMath {
 
     public static void main(String[] args) {
         MyMath myMath = new MyMath();
+        myMath.printArr(10, 9, 2, 3);
         myMath.printArr(2, 9, null);
         myMath.printArr(2, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9);
     }
